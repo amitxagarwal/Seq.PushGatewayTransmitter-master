3e104cb9e3082ee8222d86f21634ee1f9a31b223
Amit Agarwal <WXW@kmd.dk>
2019-07-31 00:57:37 +0530

    Add project files.

---
+++ Seq.PushGatewayTransmitter/PushgatewayReactor.cs
@@ -0,0 +1,77 @@
+﻿using Prometheus.Client;
+using Prometheus.Client.MetricPusher;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushgatewayTransmitter
+{
+    [SeqApp("Seq.PushgatewayReactor",
+        Description = "Uses a Handlebars template to send events to Pushgateway.")]
+    public class PushgatewayReactor : Reactor, ISubscribeTo<LogEventData>
+    {
+        //private readonly object pushGatewayNotification;
+
+        [SeqAppSetting(
+            DisplayName = "Pushgateway URL",
+            HelpText = "The URL of the Pushgateway")]
+        public static string PushgatewayUrl { get; set; }
+
+        public void On(Event<LogEventData> evt)
+        {
+            var applicationName = FormatTemplate(evt);
+            var pushGatewayUrl = "https://kmd-shareddev-monitoring.westeurope.cloudapp.azure.com/pushgateway";
+            var seqPushgatewayWorkerName = "pushgateway-testworker";
+            var instanceName = "default";
+
+            var defaultPusher = new MetricPusher(pushGatewayUrl, seqPushgatewayWorkerName, instanceName);
+            IMetricPushServer server = new MetricPushServer(new IMetricPusher[]
+            {
+                defaultPusher
+            });
+            server.Start();
+            var counter = Metrics.CreateCounter("webjobcounter", "helptext", new[] { "ApplicationName" });
+            counter.Labels(applicationName).Inc();
+            server.Stop();
+        }
+
+        public static string FormatTemplate(Event<LogEventData> evt)
+        {
+            var properties = (IDictionary<string, object>)ToDynamic(evt.Data.Properties ?? new Dictionary<string, object>());
+            var resourceName = string.Empty;
+
+            foreach (var property in properties)
+            {
+                if (property.Key == "Application")
+                {
+                    resourceName = property.Value.ToString();
+                }
+            }
+            return resourceName;
+        }
+
+        private static object ToDynamic(object o)
+        {
+            if (o is IEnumerable<KeyValuePair<string, object>> dictionary)
+            {
+                var result = new ExpandoObject();
+                var asDict = (IDictionary<string, object>)result;
+                foreach (var kvp in dictionary)
+                    asDict.Add(kvp.Key, kvp.Value);
+                return result;
+            }
+
+            if (o is IEnumerable<object> enumerable)
+            {
+                return enumerable.Select(ToDynamic).ToArray();
+            }
+
+            return o;
+        }
+    }
+}
